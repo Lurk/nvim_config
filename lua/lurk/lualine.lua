10df82a40dd3c2e049245c9b5d5ae0c9e2bfe468
Serhiy Barhamon <sergey@bargamon.ru>
2023-01-30 13:17:08 +0100

    move navic to separate section of lualine

--- lua/lurk/lualine.lua
+++ lua/lurk/lualine.lua
@@ -2,6 +2,7 @@ vim.cmd("colorscheme nightfox")
 
 require('lualine').setup {
     winbar = {
-        lualine_a = { "filename", "%{%v:lua.require'nvim-navic'.get_location()%}" },
+        lualine_a = { "filename" },
+        lualine_b = { "%{%v:lua.require'nvim-navic'.get_location()%}" }
     }
 }
